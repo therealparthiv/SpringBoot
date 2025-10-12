5b0f7542f1d46a96e46844f712f4533a8b91d647
Parthiv V Nair <parthiv@Mac.mshome.net>
2025-10-12 18:20:10 +0530

    Setting up

---
+++ src/main/java/com/example/parthiv/intro/IntroToSpringBoot/IntroToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.parthiv.intro.IntroToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/parthiv/intro/IntroToSpringBoot/IntroToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parthiv.intro.IntroToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
