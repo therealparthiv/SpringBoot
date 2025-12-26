cf46d289d0aa2b0b60735ad7c00b0e5f0f28a003
therealparthiv <writetoparthiv@gmail.com>
2025-12-26 23:12:42 +0530

    Logging

--- src/main/java/com/projects/parthiv/projection_ready_features/impl/EmployeeClientImpl.java
+++ src/main/java/com/projects/parthiv/projection_ready_features/impl/EmployeeClientImpl.java
@@ -5,14 +5,14 @@ import com.projects.parthiv.projection_ready_features.clients.EmployeeClient;
 import com.projects.parthiv.projection_ready_features.dto.EmployeeDTO;
 import com.projects.parthiv.projection_ready_features.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
-import java.lang.reflect.Type;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -21,56 +21,84 @@ public class EmployeeClientImpl implements EmployeeClient {
 
     private final RestClient restClient;
 
+    Logger log = LoggerFactory.getLogger(EmployeeClientImpl.class);
+
     @Override
     public List<EmployeeDTO> getAllEmployees() {
-        try{
-            ApiResponse<List<EmployeeDTO>> employeeDTOList= restClient
+        log.trace("Trying to retrieve all employees in getAllEmployees");
+        try {
+            log.info("Attempting to call the restClient method in getAllEmployees");
+
+            ApiResponse<List<EmployeeDTO>> employeeDTOList = restClient
                     .get()
                     .uri("employees")
                     .retrieve()
-                    .body(new ParameterizedTypeReference<>() {
-                    });
+                    .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                        log.error("Client error while fetching employees: {}",
+                                new String(res.getBody().readAllBytes()));
+                        throw new ResourceNotFoundException("could not retrieve employees");
+                    })
+                    .body(new ParameterizedTypeReference<>() {});
+
+            log.debug("Successfully retrieved employees in getAllEmployees");
+            log.trace("Retrieved Employee List in getAllEmployees: {}",
+                    employeeDTOList.getData());
 
             return employeeDTOList.getData();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
+            log.error("Exception occurred in getAllEmployees", e);
             throw new RuntimeException(e);
         }
-
     }
 
     @Override
     public EmployeeDTO getEmployeeById(Long employeeId) {
-        try{
-            ApiResponse<EmployeeDTO> employeeResponse= restClient.get()
+        log.trace("Trying to retrieve employee by id {}", employeeId);
+        try {
+            log.info("Attempting to call the restClient method in getEmployeeById for id {}",
+                    employeeId);
+
+            ApiResponse<EmployeeDTO> employeeResponse = restClient.get()
                     .uri("employees/{employeeId}", employeeId)
                     .retrieve()
-                    .body(new ParameterizedTypeReference<>() {
-                        });
+                    .body(new ParameterizedTypeReference<>() {});
+
+            log.debug("Successfully retrieved employee with id {}", employeeId);
+            log.trace("Retrieved Employee Data in getEmployeeById: {}",
+                    employeeResponse.getData());
 
             return employeeResponse.getData();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
+            log.error("Exception occurred in getEmployeeById for id {}", employeeId, e);
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public EmployeeDTO createNewEmployee(EmployeeDTO employeeDTO) {
-        try{
-            ResponseEntity<ApiResponse<EmployeeDTO>> employeeDTOApiResponse= restClient.post()
-                    .uri("employees")
-                    .body(employeeDTO)
-                    .retrieve()
-                    .onStatus(HttpStatusCode::is4xxClientError, ((request, response) -> {
-                        System.out.println(new String(response.getBody().readAllBytes()));
-                        throw new ResourceNotFoundException("Could not create the Employee");
-                    }))
-                    .toEntity(new ParameterizedTypeReference<>(){
-                    });
+        log.trace("Trying to create new employee with data {}", employeeDTO);
+        try {
+            log.info("Attempting to call the restClient method in createNewEmployee");
+
+            ResponseEntity<ApiResponse<EmployeeDTO>> employeeDTOApiResponse =
+                    restClient.post()
+                            .uri("employees")
+                            .body(employeeDTO)
+                            .retrieve()
+                            .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+                                log.error("Client error while creating employee: {}",
+                                        new String(response.getBody().readAllBytes()));
+                                throw new ResourceNotFoundException("Could not create the Employee");
+                            })
+                            .toEntity(new ParameterizedTypeReference<>() {});
+
+            log.debug("Successfully created employee in createNewEmployee");
+            log.trace("Created Employee Data in createNewEmployee: {}",
+                    employeeDTOApiResponse.getBody().getData());
 
             return employeeDTOApiResponse.getBody().getData();
         } catch (Exception e) {
+            log.error("Exception occurred in createNewEmployee", e);
             throw new RuntimeException(e);
         }
     }
--- src/test/java/com/projects/parthiv/projection_ready_features/ProjectionReadyFeaturesApplicationTests.java
+++ src/test/java/com/projects/parthiv/projection_ready_features/ProjectionReadyFeaturesApplicationTests.java
@@ -33,7 +33,7 @@ class ProjectionReadyFeaturesApplicationTests {
     @Test
     @Order(2)
     void getEmployeeByIdTest(){
-        EmployeeDTO employeeDTO= employeeClient.getEmployeeById(1L);
+        EmployeeDTO employeeDTO= employeeClient.getEmployeeById(9L);
         System.out.println(employeeDTO);;
     }
 
@@ -41,7 +41,7 @@ class ProjectionReadyFeaturesApplicationTests {
     @Order(1)
     void createNewEmployeeTest(){
         EmployeeDTO employeeDTO= new EmployeeDTO(null, "Parthiv", "parthiv@gmail.com",
-                2, "USER", 5000, LocalDate.of(2020, 12, 1), true);
+                21, "USER", 5000, LocalDate.of(2020, 12, 1), true);
         EmployeeDTO savedEmployeeDTO= employeeClient.createNewEmployee(employeeDTO);
         System.out.println(savedEmployeeDTO);
     }
